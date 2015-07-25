7d22c6b09c23f99b21f9a71a728eab1c550815ab
davidbrai <davidbrai@gmail.com>
2015-07-25 17:40:44 +0300

    changed pulse animation

--- PulseSensor.h
+++ PulseSensor.h
@@ -6,40 +6,71 @@ volatile int IBI = 600;             // holds the time between beats, must be see
 volatile boolean Pulse = false;     // true when pulse wave is high, false when it's low
 volatile boolean QS = false;        // becomes true when Arduoino finds a beat.
 
-CRGB pulsePalette[14];
+int ledNum = 0;
+int randomR = 0;
 
-void setupPulsePalette() {
-  fill_solid(pulsePalette, 14, CRGB::Black);
-  pulsePalette[0] = CRGB::Red;
-  pulsePalette[1] = CRGB::Purple;
-  pulsePalette[2] = CRGB::Red;
-  
-  pulsePalette[0+11] = CRGB::Red;
-  pulsePalette[1+11] = CRGB::Purple;
-  pulsePalette[2+11] = CRGB::Red;
-}
+void fadeLEDs(int fadeVal);
 
-int pulseRate = 60;
-unsigned long startTime = 0;
-unsigned long secondStartTime = 0;
+//================================================================================================
+// beatTriggered() : This is the LED animation sequence when the heart beats
+//================================================================================================
+void beatTriggered(){
 
-void showLedAtLocation(int ledLocation) {
-  for (int i=0; i < 14; i++) {
-    if (ledLocation - i < 0) break;
-    leds[ledLocation-i] = pulsePalette[13-i];
+  //Ignite 30 LEDs with a red value between 0 to 255
+  for(int i = 0; i<30; i++){
+    //The red channel is randomised to a value between 0 to 255
+    leds[ledNum].r=random8();
+//    FastLED.show();
+
+    //Call the fadeLEDs method after every 3rd LED is lit.
+    if(ledNum%3==0){
+      fadeLEDs(5);
+    }
+
+    //Move to the next LED
+    ledNum++;
+
+    //Make sure to move back to the beginning if the animation falls off the end of the strip
+    if(ledNum>(NUM_LEDS-1)){
+      ledNum=0;
+    }
+  }
+
+  //Ignite 20 LEDS with a blue value between 0 to 120
+  for(int i = 0; i<20; i++){
+    //The blue channel is randomised to a value between 0 to 120
+    leds[ledNum].b=random8(120);
+
+    //Call the fadeLEDs method after every 3rd LED is lit.
+    if(ledNum%3==0){
+      fadeLEDs(5);
+    }
+
+    //Move to the next LED
+    ledNum++;
+
+    //Make sure to move back to the beginning if the animation falls off the end of the strip
+    if(ledNum>(NUM_LEDS-1)){
+      ledNum=0;
+    }
   }
 }
 
-unsigned long showLedsWithStartTime(long startTime) {
-  int millisPerLocation = 20;
-  if (startTime > 0) {
-    unsigned long millisPassed = millis() - startTime;
-    long ledLocation = (millisPassed / millisPerLocation);
-    if (ledLocation <  NUM_LEDS) {
-      showLedAtLocation(ledLocation);
-      return startTime;
-    } else {
-      return 0;
+void fadeLEDs(int fadeVal){
+  for (int i = 0; i<NUM_LEDS; i++){
+    //Fade every LED by the fadeVal amount
+    leds[i].fadeToBlackBy(fadeVal);
+
+    //Randomly re-fuel some of the LEDs that are currently lit (1% chance per cycle)
+    //This enhances the twinkling effect.
+    if(leds[i].r>10){
+      randomR = random8(100);
+      if(randomR<1){
+        //Set the red channel to a value of 80
+        leds[i].r=80;
+        //Increase the green channel to 20 - to add to the effect
+        leds[i].g=20;
+      }
     }
   }
 }
@@ -47,28 +78,20 @@ unsigned long showLedsWithStartTime(long startTime) {
 uint8_t pulseSensor(uint8_t arg0, uint8_t arg1) {
 //  PRINTX("Signal: ", Signal);
   if (QS) {
-    Serial.println("Got QS");
-    secondStartTime = startTime;
-    startTime = millis();
+    PRINT("Got QS");
     QS = false;
+
+    beatTriggered();
   }
 
-//  Serial.println(startTime);
-
-  FastLED.clear();
-  startTime = showLedsWithStartTime(startTime);
-
-//  f_show();
-  FastLED.show();
-
-//  FastLED.delay(10);
+  fadeLEDs(5);
   
-  return 10;
+  return NO_DELAY;
 }
+
 
 void setupPulseSensor() {
   pinMode(blinkPin,OUTPUT);
-  setupPulsePalette();
   interruptSetup();
 }
 
